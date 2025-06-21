3596d454185ff21d59e2f9d0b9b32d888b5c9d33
MohammedNowfel <md.nowfel29@gmail.com>
2025-06-21 16:08:22 +0530

    python file has created

---
+++ arithmetic.py
@@ -0,0 +1,16 @@
+a = 20
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) # it gives float value
+print(a%b)
+print(a//b)# it is called floor division it gives integer value
+print(a**b)
+
+#quiz
+
+print(10+5*3)
+print(20-10/5*2)
+
+print(2**3**(2%10))
---
+++ relational.py
@@ -0,0 +1,21 @@
+a = 20
+b = 5
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b) 
+
+
+#quiz
+
+a = "Siva"
+b = "siva"
+print(a==b)
+
+
+#ascii value
+print('S:',ord('S'))
+print('s:',ord('s'))
+print(a<b)
