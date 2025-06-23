d6f39d4dec992b431b52f28b43ffffe3402124eb
MohammedNowfel <md.nowfel29@gmail.com>
2025-06-23 19:17:33 +0530

    python file has created

---
+++ logical.py
@@ -0,0 +1,21 @@
+#Logical Operators
+
+a = 5
+b = 10
+
+statement1 = ( a == 10 )
+statement2 = ( b == 10 )
+
+print(statement1 or statement2)
+print(statement1 and statement2)
+
+print(not(False))
+print(not(True))
+
+
+#quiz
+
+num = 10
+print(num>5 and num<15)
+print(num % 2 == 0 or num % 3 ==0)
+print(not(num == 0))
---
+++ membership.py
@@ -0,0 +1,15 @@
+lst = [1,2,3,4,5] #list
+name = "Mohammed Nowfel"
+age = 19
+
+print(3 in lst)
+print('s' not in name)
+
+# print(9 in age) // membership operator is not used in the integer value
+
+
+#quiz
+
+numbers = [1,4,'2',8,5]
+print(2 in numbers)
+print(10 not in numbers)
