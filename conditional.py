f057bd616fab1598a3bf43935b7e30d2e9d214eb
MohammedNowfel <md.nowfel29@gmail.com>
2025-07-03 19:40:09 +0530

    python file has created

---
+++ conditional.py
@@ -0,0 +1,13 @@
+a = input("Enter a number: ")
+b = input("Enter another number: ")
+
+if(a==b):
+    print("a is equal to b");
+
+elif(a>b):
+    print("a is greater then b");
+
+else:
+    print("a is less than b");
+
+
---
+++ conditional_Eg.py
@@ -0,0 +1,12 @@
+# Positive or Negative numbers
+
+a = int(input("Enter a number: "));
+
+if(a>0):
+    print("Positive Number");
+
+elif(a<0):
+    print("Negative Number");
+
+else:
+    print("Zero");
