c0ce060fab01b9f58545d6e57bcc66480bd9863b
MohammedNowfel <md.nowfel29@gmail.com>
2025-07-17 17:00:32 +0530

    python file has created

---
+++ list.py
@@ -0,0 +1,15 @@
+# List in python
+
+height_list = [170, 180, 175, 160, 165, "Hii"] # it is heterogeneous list. it allows different data types
+print(height_list)
+
+lst_1 = [1,2,3,4,5]
+lst_2 = [6,7,8,9,10]
+
+lst_add = lst_1 + lst_2  # merge two lists
+
+lst_mul = lst_1 * 2  # repeat list
+
+print(lst_add) 
+
+print(lst_mul) 
