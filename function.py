9c9c1a69441b582f1eeac5b720c8c85975036aad
MohammedNowfel <md.nowfel29@gmail.com>
2025-09-02 16:50:51 +0530

    python file has created

---
+++ function.py
@@ -0,0 +1,13 @@
+# Functions
+
+def hello():
+    for i in range(100):
+        print("hello world")
+hello()
+
+def blah():
+    print("blah blah blah")
+
+blah()
+hello()
+blah()
---
+++ function_eg.py
@@ -0,0 +1,7 @@
+# Quiz
+
+def greet():
+    name = input("Enter your name:")
+    print("Good Morning!", name)
+
+greet()
