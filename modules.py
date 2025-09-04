ccc00cf9ce5285068ca7389786666586191c20c9
MohammedNowfel <md.nowfel29@gmail.com>
2025-09-04 22:01:56 +0530

    python file has created

---
+++ modules.py
@@ -0,0 +1,8 @@
+# Modules
+
+import function_eg
+# from function_eg import greet
+# from function_eg import greet, add
+# from function_eg import *
+
+function_eg.greet()
---
+++ package_calc.py
@@ -0,0 +1,5 @@
+from package import add, sub, mul
+
+print(add.adder(5,3))
+print(sub.subtract(5,3))
+print(mul.multiple(5,3))
